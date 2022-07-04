d092656c05be19d2640557368ffe4c73d5194228
hujiangjun <1217437592@qq.com>
2022-07-04 11:16:12 +0800

    fix: layout

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="flex min-w-0 flex-1 mt-10" ref="wrapper">
     <div
-      class="order-1 mx-auto min-w-0 max-w-[800px] flex-1"
+      class="order-1 mx-8 min-w-0 mb-10 max-w-[800px] flex-1"
       @scroll="handleScroll"
     >
       <div class="md-head" ref="mdHead">
-        <div  v-if="attributes && attributes.coverImg" class="h-80 ">
-           <img
-          ref="img"
-          :src="attributes.coverImg"
-          :alt="attributes.title"
-          class="object-cover rounded-md h-full w-full mb-5"
-        />
+        <div v-if="attributes && attributes.coverImg" class="h-80">
+          <img
+            ref="img"
+            :src="attributes.coverImg"
+            :alt="attributes.title"
+            class="object-cover rounded-md h-full w-full mb-5"
+          />
         </div>
-       
       </div>
       <article
         class="
@@ -30,16 +29,13 @@
     <div class="z-10 order-2 hidden w-64 min-w-0 shrink-0 xl:block xl:pl-8">
       <div class="sticky top-[112px] max-h-screen overflow-y-auto">
         <div class="anchor-box font-sans text-sm">
-          <div
-            class="anchor-item"
-            v-for="(item, index) in toc"
-            :key="index"
-            :class="getClassName(item, index)"
-          >
-            <a class="anchor-item-title" :href="`#${decode(item.content)}`">
-              <span v-html="decode(item.content)"></span>
-            </a>
-          </div>
+          <template v-for="(item, index) in toc" :key="index">
+            <div v-if="item.level <= 2" class="anchor-item py-1" :class="getClassName(item, index)">
+              <a class="anchor-item-title" :href="`#${decode(item.content)}`">
+                <span v-html="decode(item.content)"></span>
+              </a>
+            </div>
+          </template>
         </div>
       </div>
     </div>
@@ -53,14 +49,14 @@ import { decode } from "html-entities";
 const props = defineProps({
   content: {
     type: String,
-    required: true
+    required: true,
   },
   attributes: {
-    type: Object
+    type: Object,
   },
   toc: {
-    type: Object
-  }
+    type: Object,
+  },
 });
 
 const activeTitle = ref(null);
